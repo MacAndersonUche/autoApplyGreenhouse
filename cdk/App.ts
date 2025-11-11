65ad484226cfa2fa59da2d6cbf5b83a375a64b87
MacAnderson Uche <macandersonuche@yahoo.com>
2025-11-11 23:13:57 +0000

    edit

--- cdk/App.ts
+++ cdk/App.ts
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import { App } from 'aws-cdk-lib';
-import { ApiStack } from '../lib/api-stack';
+import { ApiStack } from './lib/api-stack';
 
 const app = new App();
 
 new ApiStack(app, 'GreenhouseBotApiStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION || 'us-east-1',
+    region: process.env.CDK_DEFAULT_REGION || 'eu-north-1',
   },
   openaiApiKey: process.env.OPENAI_API_KEY,
 });
